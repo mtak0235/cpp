d88594d093f102a8dd2c1bee26891dcd2328e2e3
탁민경 <mtak@tagmingyeong-ui-MacBookPro.local>
2021-09-27 04:17:34 +0900

    08 pc

--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,42 +6,35 @@
 /*   By: mtak <mtak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 16:19:07 by mtak              #+#    #+#             */
-/*   Updated: 2021/09/23 16:19:11 by mtak             ###   ########.fr       */
+/*   Updated: 2021/09/27 04:17:10 by mtak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mutantstack.hpp"
 #include <iostream>
+#include <list>
 
 int main()
 {
 	MutantStack<int> mstack;
-	
 	mstack.push(5);
 	mstack.push(17);
-	
 	std::cout << mstack.top() << std::endl;
-	
 	mstack.pop();
-	
 	std::cout << mstack.size() << std::endl;
-	
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-	//[...]
 	mstack.push(0);
-	
-	// MutantStack<int>::iterator it = mstack.begin();
-	// MutantStack<int>::iterator ite = mstack.end();
-	
-	// ++it;
-	// --it;
-	// while (it != ite)
-	// {
-	// 	std::cout << *it << std::endl;
-	// 	++it;
-	// }
-	// std::stack<int> s(mstack);
-	return 0;
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return (0);
 }
--- 08/ex02/mutantstack.hpp
+++ 08/ex02/mutantstack.hpp
@@ -6,12 +6,12 @@
 /*   By: mtak <mtak@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 16:19:43 by mtak              #+#    #+#             */
-/*   Updated: 2021/09/23 16:19:56 by mtak             ###   ########.fr       */
+/*   Updated: 2021/09/27 04:06:32 by mtak             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MUTANTSTACK_H
-#define MUTANTSTACK_H
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
 
 #include <stack>
 
@@ -20,13 +20,42 @@ class MutantStack : public std::stack<T>
 {
 
 	public:
-		MutantStack<T>(); //== MutantStack();
-		MutantStack<T>(MutantStack const &);
-		~MutantStack<T>();
-		MutantStack operator = (MutantStack const &);
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		MutantStack();
+		MutantStack(MutantStack<T> const &target);
+		MutantStack<T> operator=(MutantStack<T> const &target);
+		iterator end();
+		iterator begin();
+		virtual ~MutantStack();
 };
+template <typename T> 
+MutantStack<T>::MutantStack()
+{}
 
-#include "mutantstack.cpp"	//WHY?
-							//https://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file
-							//https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl
+template <typename T> 
+MutantStack<T>::~MutantStack()
+{}
+
+template <typename T>
+MutantStack<T>::MutantStack(MutantStack<T> const & target)
+{
+	*this = target;
+}
+
+template <typename T>
+MutantStack<T> MutantStack<T>::operator=(MutantStack<T> const & target)
+{
+	if (this != &target)
+		*this = target;
+	return *this;
+}
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{ return (this->c.begin()); }
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{ return (this->c.end()); }
+
 #endif
